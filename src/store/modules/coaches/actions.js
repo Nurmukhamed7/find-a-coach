1975597b53149faacc8c334c56ae5c1abaa3f345
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-10-30 17:42:32 +0100

    Fetched Coach Data (Get HTTP Request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,4 +27,31 @@ export default {
 			id: userId,
 		})
 	},
+	async loadCoaches(context) {
+		const response = await fetch(
+			'https://vue-http-demo-58928-default-rtdb.europe-west1.firebasedatabase.app/coaches.json'
+		)
+		const responseData = await response.json()
+
+		if (!response.ok) {
+			// error
+		}
+
+		const coaches = []
+
+		for (const key in responseData) {
+			const coach = {
+				id: key,
+				firstName: responseData[key].firstName,
+				lastName: responseData[key].lastName,
+				areas: responseData[key].areas,
+				description: responseData[key].lastName,
+				hourlyRate: responseData[key].hourlyRate,
+			}
+			console.log(responseData[key])
+			coaches.push(coach)
+		}
+
+		context.commit('setCoaches', coaches)
+	},
 }

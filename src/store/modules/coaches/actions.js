68414679a82d69b57336bbe55d768c2080264652
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-10-30 15:54:14 +0100

    Added BackEnd. Stored CoachData

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-	registerCoach(context, data) {
+	async registerCoach(context, data) {
+		const userId = context.rootGetters.userId
 		const coachData = {
-			id: context.rootGetters.userId,
 			firstName: data.first,
 			lastName: data.last,
 			areas: data.areas,
@@ -9,6 +9,22 @@ export default {
 			hourlyRate: data.rate,
 		}
 
-		context.commit('registerCoach', coachData)
+		const response = await fetch(
+			`https://vue-http-demo-58928-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+			{
+				method: 'PUT',
+				body: JSON.stringify(coachData),
+			}
+		)
+		// const responseData = await response.json()
+
+		if (!response.ok) {
+			// error
+		}
+
+		context.commit('registerCoach', {
+			...coachData,
+			id: userId,
+		})
 	},
 }
